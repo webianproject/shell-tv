d465c4b98b6f3c35f25c83e527b82ce95dd09731
Ben Francis <ben@tola.me.uk>
2019-11-28 13:22:54 +0000

    Fix path for static assets

--- services/router.js
+++ services/router.js
@@ -1,15 +1,16 @@
 /**
  * Router routes HTTP requests to appropriate controllers.
  */
-var express = require('express');
-var router = express.Router();
-var actionsController = require('./controllers/actions-controller');
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const actionsController = require('./controllers/actions-controller');
 
 // Use JSON middleware
 router.use(express.json());
 // Serve action requests
 router.use('/actions', actionsController);
 // Serve static files
-router.use(express.static('./services/static'));
+router.use(express.static(path.join(__dirname, 'static')));
 
 module.exports = router;
